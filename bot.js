26a03f9504b2811db5f6dad533dd751068561843
medo67 <45091642+medo67@users.noreply.github.com>
2019-02-02 11:31:14 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ var prefix = "$";
 var adminprefix = '$'
 
 
-const developers = ["ID","ID"]
+const developers = ["ID","538794966756098057"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
